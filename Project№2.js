fdfb80384721efdd57c409149366bd42200c62a8
Bacejluck <119012783+Bacejluck@users.noreply.github.com>
2023-04-23 16:14:46 +0300

    Project 2
    заменил var на let

--- Project№2.js
+++ Project№2.js
@@ -1,16 +1,16 @@
-var fib=[1,2];
+let fib=[1,2];
 
 function sumEvenFib(max){
 while(fib[fib.length-1]<max){
- var next= fib[fib.length-1]+ fib[fib.length-2];
+ let next= fib[fib.length-1]+ fib[fib.length-2];
  fib.push(next);
 }
 
-var evenFib= fib.filter(function(number){
+let evenFib= fib.filter(function(number){
 return number%2===0;
 });
 
-var sum= evenFib.reduce(function(a,b){
+let sum= evenFib.reduce(function(a,b){
     return a+b;   
 });
 console.log(sum);
---
+++ Project№3.js
@@ -0,0 +1,9 @@
+let x=0;
+while(x<10){
+let a;
+a=x+1
+x++
+console.log(a)
+}
+let b;
+let le;
