2c8389cda8e9ba141d2dcfa339c40964c561faaa
Bacejluck <119012783+Bacejluck@users.noreply.github.com>
2023-04-23 17:29:01 +0300

    Project 2
    добавил комментарии

--- Project№2.js
+++ Project№2.js
@@ -1,16 +1,16 @@
 let fib=[1,2];
 
-function sumEvenFib(max){
+function sumEvenFib(max){// создаем список ,путем сложения двух его последних элементов 
 while(fib[fib.length-1]<max){
  let next= fib[fib.length-1]+ fib[fib.length-2];
  fib.push(next);
 }
 
-let evenFib= fib.filter(function(number){
+let evenFib= fib.filter(function(number){// создаем переменную которая в качестве значения принимает спикос с четными элементами
 return number%2===0;
 });
 
-let sum= evenFib.reduce(function(a,b){
+let sum= evenFib.reduce(function(a,b){//сумируем элементы массива путем метода reduce
     return a+b;   
 });
 console.log(sum);
